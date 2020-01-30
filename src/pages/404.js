03ac6da71816abb8b4251833fb81c3665300e072
Alicia collymore <hello@aliciacollymore.com>
2020-01-30 20:30:40 +0000

    updated 404 page

--- src/pages/404.js
+++ src/pages/404.js
@@ -1,10 +1,21 @@
 import React from 'react';
 import Layout from '../components/layout';
+import Footer from '../components/Footer';
+import Header from '../components/Header';
+import { Link } from 'gatsby';
 
 const NotFoundPage = () => (
   <Layout>
-    <h1>NOT FOUND</h1>
-    <p>Not a valid URL</p>
+     <Header />
+     <div id="wrapper">
+      <section id="main" className="wrapper">
+        <div className="inner">
+          <h1 className="major">404 page not found</h1>
+
+          <p>Looks like this page does not exist, head over to our <Link to="/">home page</Link> to get back on track. </p>
+        </div>
+      </section>
+    </div>
   </Layout>
 );
 
